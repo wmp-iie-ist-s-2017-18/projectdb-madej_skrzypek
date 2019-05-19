a5143a7ee8872965743c99942fcbb04ecea2eea4
Essasito <37266446+Essasito@users.noreply.github.com>
2019-05-19 23:09:14 +0200

    zrobilem przyciski dla klienta i polaczylem tabele

---
+++ src/Apteka/KlientJpaController.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Apteka;
+
+import Apteka.exceptions.IllegalOrphanException;
+import Apteka.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Sebcio
+ */
+public class KlientJpaController implements Serializable {
+
+    public KlientJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Klient klient) {
+        if (klient.getReceptaCollection() == null) {
+            klient.setReceptaCollection(new ArrayList<Recepta>());
+        }
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Collection<Recepta> attachedReceptaCollection = new ArrayList<Recepta>();
+            for (Recepta receptaCollectionReceptaToAttach : klient.getReceptaCollection()) {
+                receptaCollectionReceptaToAttach = em.getReference(receptaCollectionReceptaToAttach.getClass(), receptaCollectionReceptaToAttach.getIDrecepty());
+                attachedReceptaCollection.add(receptaCollectionReceptaToAttach);
+            }
+            klient.setReceptaCollection(attachedReceptaCollection);
+            em.persist(klient);
+            for (Recepta receptaCollectionRecepta : klient.getReceptaCollection()) {
+                Klient oldKlientIDOfReceptaCollectionRecepta = receptaCollectionRecepta.getKlientID();
+                receptaCollectionRecepta.setKlientID(klient);
+                receptaCollectionRecepta = em.merge(receptaCollectionRecepta);
+                if (oldKlientIDOfReceptaCollectionRecepta != null) {
+                    oldKlientIDOfReceptaCollectionRecepta.getReceptaCollection().remove(receptaCollectionRecepta);
+                    oldKlientIDOfReceptaCollectionRecepta = em.merge(oldKlientIDOfReceptaCollectionRecepta);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Klient klient) throws IllegalOrphanException, NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Klient persistentKlient = em.find(Klient.class, klient.getIDKlienta());
+            Collection<Recepta> receptaCollectionOld = persistentKlient.getReceptaCollection();
+            Collection<Recepta> receptaCollectionNew = klient.getReceptaCollection();
+            List<String> illegalOrphanMessages = null;
+            for (Recepta receptaCollectionOldRecepta : receptaCollectionOld) {
+                if (!receptaCollectionNew.contains(receptaCollectionOldRecepta)) {
+                    if (illegalOrphanMessages == null) {
+                        illegalOrphanMessages = new ArrayList<String>();
+                    }
+                    illegalOrphanMessages.add("You must retain Recepta " + receptaCollectionOldRecepta + " since its klientID field is not nullable.");
+                }
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            Collection<Recepta> attachedReceptaCollectionNew = new ArrayList<Recepta>();
+            for (Recepta receptaCollectionNewReceptaToAttach : receptaCollectionNew) {
+                receptaCollectionNewReceptaToAttach = em.getReference(receptaCollectionNewReceptaToAttach.getClass(), receptaCollectionNewReceptaToAttach.getIDrecepty());
+                attachedReceptaCollectionNew.add(receptaCollectionNewReceptaToAttach);
+            }
+            receptaCollectionNew = attachedReceptaCollectionNew;
+            klient.setReceptaCollection(receptaCollectionNew);
+            klient = em.merge(klient);
+            for (Recepta receptaCollectionNewRecepta : receptaCollectionNew) {
+                if (!receptaCollectionOld.contains(receptaCollectionNewRecepta)) {
+                    Klient oldKlientIDOfReceptaCollectionNewRecepta = receptaCollectionNewRecepta.getKlientID();
+                    receptaCollectionNewRecepta.setKlientID(klient);
+                    receptaCollectionNewRecepta = em.merge(receptaCollectionNewRecepta);
+                    if (oldKlientIDOfReceptaCollectionNewRecepta != null && !oldKlientIDOfReceptaCollectionNewRecepta.equals(klient)) {
+                        oldKlientIDOfReceptaCollectionNewRecepta.getReceptaCollection().remove(receptaCollectionNewRecepta);
+                        oldKlientIDOfReceptaCollectionNewRecepta = em.merge(oldKlientIDOfReceptaCollectionNewRecepta);
+                    }
+                }
+            }
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = klient.getIDKlienta();
+                if (findKlient(id) == null) {
+                    throw new NonexistentEntityException("The klient with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws IllegalOrphanException, NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Klient klient;
+            try {
+                klient = em.getReference(Klient.class, id);
+                klient.getIDKlienta();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The klient with id " + id + " no longer exists.", enfe);
+            }
+            List<String> illegalOrphanMessages = null;
+            Collection<Recepta> receptaCollectionOrphanCheck = klient.getReceptaCollection();
+            for (Recepta receptaCollectionOrphanCheckRecepta : receptaCollectionOrphanCheck) {
+                if (illegalOrphanMessages == null) {
+                    illegalOrphanMessages = new ArrayList<String>();
+                }
+                illegalOrphanMessages.add("This Klient (" + klient + ") cannot be destroyed since the Recepta " + receptaCollectionOrphanCheckRecepta + " in its receptaCollection field has a non-nullable klientID field.");
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            em.remove(klient);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Klient> findKlientEntities() {
+        return findKlientEntities(true, -1, -1);
+    }
+
+    public List<Klient> findKlientEntities(int maxResults, int firstResult) {
+        return findKlientEntities(false, maxResults, firstResult);
+    }
+
+    private List<Klient> findKlientEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Klient.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Klient findKlient(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Klient.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getKlientCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Klient> rt = cq.from(Klient.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/Apteka/MagazynLeki.java
@@ -0,0 +1,188 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Apteka;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Sebcio
+ */
+@Entity
+@Table(name = "magazyn_leki")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "MagazynLeki.findAll", query = "SELECT m FROM MagazynLeki m")
+    , @NamedQuery(name = "MagazynLeki.findByIDleku", query = "SELECT m FROM MagazynLeki m WHERE m.iDleku = :iDleku")
+    , @NamedQuery(name = "MagazynLeki.findByNazwa", query = "SELECT m FROM MagazynLeki m WHERE m.nazwa = :nazwa")
+    , @NamedQuery(name = "MagazynLeki.findByCena", query = "SELECT m FROM MagazynLeki m WHERE m.cena = :cena")
+    , @NamedQuery(name = "MagazynLeki.findByIlosc", query = "SELECT m FROM MagazynLeki m WHERE m.ilosc = :ilosc")
+    , @NamedQuery(name = "MagazynLeki.findByDatawaznosci", query = "SELECT m FROM MagazynLeki m WHERE m.datawaznosci = :datawaznosci")})
+public class MagazynLeki implements Serializable {
+
+    @Basic(optional = false)
+    @Column(name = "Cena")
+    private long cena;
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_leku")
+    private Integer iDleku;
+    @Basic(optional = false)
+    @Column(name = "Nazwa")
+    private String nazwa;
+    @Basic(optional = false)
+    @Column(name = "Ilosc")
+    private int ilosc;
+    @Basic(optional = false)
+    @Column(name = "Data_waznosci")
+    @Temporal(TemporalType.DATE)
+    private Date datawaznosci;
+    @ManyToMany(mappedBy = "magazynLekiCollection")
+    private Collection<Recepta> receptaCollection;
+    @ManyToMany(mappedBy = "magazynLekiCollection")
+    private Collection<Dostawa> dostawaCollection;
+
+    public MagazynLeki() {
+    }
+
+    public MagazynLeki(Integer iDleku) {
+        this.iDleku = iDleku;
+    }
+
+    public MagazynLeki(Integer iDleku, String nazwa, int cena, int ilosc, Date datawaznosci) {
+        this.iDleku = iDleku;
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.ilosc = ilosc;
+        this.datawaznosci = datawaznosci;
+    }
+
+    public Integer getIDleku() {
+        return iDleku;
+    }
+
+    public void setIDleku(Integer iDleku) {
+        Integer oldIDleku = this.iDleku;
+        this.iDleku = iDleku;
+        changeSupport.firePropertyChange("IDleku", oldIDleku, iDleku);
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        String oldNazwa = this.nazwa;
+        this.nazwa = nazwa;
+        changeSupport.firePropertyChange("nazwa", oldNazwa, nazwa);
+    }
+
+
+    public int getIlosc() {
+        return ilosc;
+    }
+
+    public void setIlosc(int ilosc) {
+        int oldIlosc = this.ilosc;
+        this.ilosc = ilosc;
+        changeSupport.firePropertyChange("ilosc", oldIlosc, ilosc);
+    }
+
+    public Date getDatawaznosci() {
+        return datawaznosci;
+    }
+
+    public void setDatawaznosci(Date datawaznosci) {
+        Date oldDatawaznosci = this.datawaznosci;
+        this.datawaznosci = datawaznosci;
+        changeSupport.firePropertyChange("datawaznosci", oldDatawaznosci, datawaznosci);
+    }
+
+    @XmlTransient
+    public Collection<Recepta> getReceptaCollection() {
+        return receptaCollection;
+    }
+
+    public void setReceptaCollection(Collection<Recepta> receptaCollection) {
+        this.receptaCollection = receptaCollection;
+    }
+
+    @XmlTransient
+    public Collection<Dostawa> getDostawaCollection() {
+        return dostawaCollection;
+    }
+
+    public void setDostawaCollection(Collection<Dostawa> dostawaCollection) {
+        this.dostawaCollection = dostawaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (iDleku != null ? iDleku.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MagazynLeki)) {
+            return false;
+        }
+        MagazynLeki other = (MagazynLeki) object;
+        if ((this.iDleku == null && other.iDleku != null) || (this.iDleku != null && !this.iDleku.equals(other.iDleku))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Apteka.MagazynLeki[ iDleku=" + iDleku + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+
+    public long getCena() {
+        return cena;
+    }
+
+    public void setCena(long cena) {
+        this.cena = cena;
+    }
+    
+}
